31fca744f00cf5c8d2d592485b5083fb90e0b40c
Sir <2573506037@qq.com>
2018-11-27 16:16:56 +0800

    新建项目

---
+++ src/test/new_hello.java
@@ -0,0 +1,8 @@
+package test;
+
+public class new_hello {
+    public static void main(String[] args)
+    {
+        System.out.println("我是新来的.........");
+    }
+}
--- src/test/test_add.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class test_add {
-    public static void main(String[] args)
-    {
-        System.out.println("新加入github里边的");
-    }
-}
